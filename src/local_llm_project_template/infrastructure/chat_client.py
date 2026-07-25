e31109ce4b9494d6cbfb7f4dca3167489718cfdc
Alexey Nilov <alexeyn00000@gmail.com>
2026-07-25 08:48:10 +0200

    add chat

--- src/local_llm_project_template/infrastructure/chat_client.py
+++ src/local_llm_project_template/infrastructure/chat_client.py
@@ -3,6 +3,6 @@ from openai import AsyncOpenAI
 from local_llm_project_template.config import LLM_API_KEY, LLM_BASE_URL
 
 chat_client = AsyncOpenAI(
-    api_key=LLM_BASE_URL,
-    base_url=LLM_API_KEY,
+    api_key=LLM_API_KEY,
+    base_url=LLM_BASE_URL,
 )
